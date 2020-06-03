5492059b4a3dae7ed3e600fdb9329062eeb086ab
yeniendah <yndah02@gmail.com>
2020-06-03 09:59:39 +0700

    tugas keempat

---
+++ app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php
+
+class User_model{
+    private $nama = 'Yeni Endah';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
---
+++ app/views/templates/footer.php
@@ -0,0 +1,6 @@
+<script src="<?= BASEURL; ?>/jquery/jquery-3.3.1.min.js"></script>
+<script src="<?= BASEURL; ?>/popper/popper.min.js"></script>
+<script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
+<script src="<?= BASEURL; ?>/js/script.js"></script>
+</body>
+</html>
